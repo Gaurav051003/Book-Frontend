45472c4e96e61d4b05eeb2cf855e15750b927c7e
Gaurav051003 <gauravaryasirohi@gmail.com>
2025-05-09 17:39:27 +0530

    add

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -8,7 +8,7 @@ function Course() {
     const getBook = async () => {
       try {
         const res = await axios.get("https://books-backend-mzmk.onrender.com/book");
-        console.log(res.data);
+        // console.log(res.data);
         setBook(res.data);
       } catch (error) {
         console.log(error);
