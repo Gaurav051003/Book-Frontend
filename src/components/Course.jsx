b1683b342ead379b78dbc74721bc07a3693da588
Gaurav051003 <gauravaryasirohi@gmail.com>
2025-05-09 17:52:52 +0530

    update

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -7,7 +7,7 @@ function Course() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("https://books-backend-mzmk.onrender.com/book");
+        const res = await axios.get("https://books-backend-mzmk.onrender.com/");
         // console.log(res.data);
         setBook(res.data);
       } catch (error) {
