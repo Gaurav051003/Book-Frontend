231c100e89c342a72937754c9ac04af75ad6ec7f
Gaurav051003 <136368404+Gaurav051003@users.noreply.github.com>
2025-05-10 10:50:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Signup from "./components/Signup";
 import { Toaster } from "react-hot-toast";
 import { useAuth } from "./context/AuthProvider";
+import Courses from "./courses/Courses";
 
 
 function App() {
@@ -14,7 +15,10 @@ function App() {
       <div className="dark:bg-slate-900 dark:text-white">
         <Routes>
           <Route path="/" element={<Home />} />
-  
+          <Route
+            path="/book"
+            element={authUser ? <Courses /> : <Navigate to="/signup" />}
+          />
           <Route path="/signup" element={<Signup />} />
         </Routes>
         <Toaster />
